eb4a8ab3b3878a0f467e46c6cf0e2d8ae582878e
Kornel Ciosek <kornel.ciosek@gmail.com>
2023-01-30 22:41:47 +0100

    Input data validations

--- rsds/app/models/facility.rb
+++ rsds/app/models/facility.rb
@@ -1,4 +1,14 @@
 class Facility < ApplicationRecord
   belongs_to :company
   has_many :courts
+
+  validates :facilityName, presence: true, uniqueness: false, length: { in: 3..50 }, format: { with: /\A[A-Za-z]+(?: [a-zA-Z]+)*\z/, message: "must start with capital letter, letters only" }
+  validates :city, presence: true, uniqueness: false, length: { in: 3..50 }, format: { with: /\A[A-Za-z]+(?: [a-zA-Z]+)*\z/, message: "only letters and spaces" }
+  validates :street, presence: true, uniqueness: false, length: { in: 3..50 }, format: { with: /\A[0-9A-Za-z]+(?: [0-9a-zA-Z]+)*\z/, message:"only letters, digits and spaces" }
+  validates :buildingNumber, presence: true, uniqueness: false, length: { maximum: 5 }
+  validates :localNumber, presence: true, uniqueness: false, length: { maximum: 5 }, format: { with: /\A[0-9]+[a-z]{1}\z/, message: "only digits and letters" }
+  validates :postalCode, presence: true, uniqueness: false, format: { with: /\A[0-9]{3}-[0-9]{2}\z/, message: "must look like 00-000" }
+  validates :openingTime, presence: true, uniqueness: false
+  validates :closingTime, presence: true, uniqueness: false, comparison: { greater_than: :closingTime }
+  validates :houerlyPrice, presence: true, uniqueness: false, length: { maximum: 5 }
 end
--- rsds/app/models/reservation.rb
+++ rsds/app/models/reservation.rb
@@ -3,4 +3,9 @@ class Reservation < ApplicationRecord
     has_many :transactions
     has_many :players
     has_many :users, through: :players
+
+    validates :reservationDate, presence: true, uniqueness: false
+    validates :reservationStartTime, presence: true, uniqueness: false, comparison: { greater_than: DateTime.now }
+    validates :reservationEndTime, presence: true, uniqueness: false, comparison: { greater_than: :reservationStartTime }
+    validates :cost, presence: true, uniqueness: false, length: { maximum: 5 }
 end
