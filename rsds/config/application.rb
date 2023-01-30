db376d7258ccc563c96031a822b720466052fda7
Kornel Ciosek <kornel.ciosek@gmail.com>
2023-01-30 16:10:39 +0100

    Allow connection by domain name

--- rsds/config/application.rb
+++ rsds/config/application.rb
@@ -11,6 +11,11 @@ module Rsds
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+
+    Rails.application.config.hosts << "rsds.lenrok.it"
+    Rails.application.config.hosts << "vscode.lenrok.it"
+
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
