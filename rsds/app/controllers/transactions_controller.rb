cb534cae71ed1850b7c8bf5e50a13a455816d33d
Kornel Ciosek <kornel.ciosek@gmail.com>
2023-01-31 14:11:51 +0100

    Fixed error in transaction

--- rsds/app/controllers/transactions_controller.rb
+++ rsds/app/controllers/transactions_controller.rb
@@ -65,6 +65,6 @@ class TransactionsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def transaction_params
-      params.require(:transaction).permit(:transaction, :value)
+      params.require(:transaction).permit(:transactionDate, :value)
     end
 end
--- rsds/app/views/transactions/_transaction.json.jbuilder
+++ rsds/app/views/transactions/_transaction.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! transaction, :id, :transaction, :value, :created_at, :updated_at
+json.extract! transaction, :id, :transactionDate, :value, :created_at, :updated_at
 json.url transaction_url(transaction, format: :json)
